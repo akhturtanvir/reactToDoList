fff91ada838c7fc05e2a65ea78982e1664d9b7b9
Md. Akhtur Uz Zaman <akhturtanvir@gmail.com>
2020-01-29 22:17:57 +0600

    Updates

---
+++ precache-manifest.5809332b494373638cb208e4580c208a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48cfaed8ed3664aabc6060f108878bac",
+    "url": "/reactToDoList/index.html"
+  },
+  {
+    "revision": "4f8bfc7d5b70c32b1f9d",
+    "url": "/reactToDoList/static/css/main.3f3b1482.chunk.css"
+  },
+  {
+    "revision": "bbb5a38eefcbe83f12ad",
+    "url": "/reactToDoList/static/js/2.44b538b4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/reactToDoList/static/js/2.44b538b4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4f8bfc7d5b70c32b1f9d",
+    "url": "/reactToDoList/static/js/main.e774aa55.chunk.js"
+  },
+  {
+    "revision": "0470ff5cecb99baf34fb",
+    "url": "/reactToDoList/static/js/runtime-main.7ea220ee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactToDoList/static/media/logo.5d5d9eef.svg"
+  }
+]);
